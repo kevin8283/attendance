d27100b94219b08f7f7e39aaba5ad9592311721f
Kevin <keyvinkeygen@gmail.com>
2020-09-17 10:03:59 +0200

    Basic express app setup

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-//App entry point
+//External dependencies
+
+const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const app = express()
+
+//Envs configuration
+dotenv.config()
+
+//Envs variables
+const port = process.env.PORT || 5000
+const dbURI = process.env.dbURI
+
+//Database connection
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+mongoose.connect(dbURI, options, () => console.log("Connected with database"))
+
+//Express built-in middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+//Routing
+
+//Server entry point
+app.listen(port, () => console.log(`Server is running on port ${port}`))
